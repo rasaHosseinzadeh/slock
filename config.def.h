a617b43422c3b7d6f903c788bdb1bbbc56559fb9
Rasa <rasa.hosseinzadeh@gmail.com>
2021-04-25 02:08:23 -0400

    Adjust Makefile & colors and Blur.

--- config.def.h
+++ config.def.h
@@ -1,15 +1,15 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
+	[INIT] =   "#404040",     /* after initialization */
+	[INPUT] =  "#924441",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
-static const int failonclear = 1;
+static const int failonclear = 0;
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
