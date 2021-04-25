f8fa86e322bc78fd7cd03c7dc871668030dfff77
Rasa <rasa.hosseinzadeh@gmail.com>
2021-04-25 02:27:51 -0400

    Apply dwmlogo patch.

--- config.def.h
+++ config.def.h
@@ -3,7 +3,8 @@ static const char *user  = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#404040",     /* after initialization */
+	[BACKGROUND] =   "black",     /* after initialization */
+	[INIT] =   "#2d2d2d",     /* after initialization */
 	[INPUT] =  "#924441",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
@@ -13,3 +14,22 @@ static const int failonclear = 0;
 
 /* time in seconds before the monitor shuts down */
 static const int monitortime = 5;
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 75;
+static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+	/* x	y	w	h */
+	{ 0,	3,	1,	3 },
+	{ 1,	3,	2,	1 },
+	{ 0,	5,	8,	1 },
+	{ 3,	0,	1,	5 },
+	{ 5,	3,	1,	2 },
+	{ 7,	3,	1,	2 },
+	{ 8,	3,	4,	1 },
+	{ 9,	4,	1,	2 },
+	{ 11,	4,	1,	2 },
+
+};
